326b511388e34da5b54227fd30e79aa937ada0d0
thisiscindy24 <cindyannwu@gmail.com>
2025-02-13 03:21:42 +0000

    squared

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,9 +1,16 @@
 class CalculatorController < ApplicationController
   def homepage
-    render({:template => "calculator_templates/homepage"})
+    render({:template => "calculator_templates/square_new"})
   end
 
   def square_new
-    render({:template => "calculator_templates/square/new"}) 
+    render({:template => "calculator_templates/square_new"}) 
+  end
+
+  def square_results 
+    @the_num = params.fetch("users_number").to_f
+    @the_result = @the_num ** 2
+    render({:template => "calculator_templates/square_results"}) 
+    
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   get"/", {:controller => "calculator", :action => "homepage"}
   get"/square/new", {:controller => "calculator", :action => "square_new" }
+  get "/square/results", {:controller => "calculator", :action => "square_results" }
+    
+  
 end
