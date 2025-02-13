7638ef9538625761c92ad09800bfcce3c1de3cbc
thisiscindy24 <cindyannwu@gmail.com>
2025-02-13 03:29:36 +0000

    square root

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -11,6 +11,17 @@ class CalculatorController < ApplicationController
     @the_num = params.fetch("users_number").to_f
     @the_result = @the_num ** 2
     render({:template => "calculator_templates/square_results"}) 
-    
   end
+
+  def square_root_new
+    render({:template => "calculator_templates/square_root_new"}) 
+  end
+
+  def square_root_results
+    @the_num = params.fetch("users_number").to_f
+    @the_result = Math.sqrt(@the_num)
+    render({:template => "calculator_templates/square_root_results"}) 
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   get"/", {:controller => "calculator", :action => "homepage"}
   get"/square/new", {:controller => "calculator", :action => "square_new" }
   get "/square/results", {:controller => "calculator", :action => "square_results" }
+  get"square_root/new", {:controller => "calculator", :action => "square_root_new" }
+  get"square_root/results", {:controller => "calculator", :action => "square_root_results" }
+
+
     
   
 end
